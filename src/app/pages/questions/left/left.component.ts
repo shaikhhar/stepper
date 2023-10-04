a17855453df7aecb8d956c6a250af05d594d8018
shekhar <skrbasnet@gmail.com>
2023-10-04 20:37:23 +0545

    Stepper design

--- src/app/pages/questions/left/left.component.ts
+++ src/app/pages/questions/left/left.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate} from '@angular/animations';
+
 
 @Component({
   selector: 'app-left',
   templateUrl: './left.component.html',
-  styleUrls: ['./left.component.scss']
+  styleUrls: ['./left.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            style({ opacity: 0 }),
+            animate('300ms ease', 
+                    style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            style({  opacity: 1 }),
+            animate('300ms ease-out', 
+                    style({opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class LeftComponent {
+  visibleStep = 1;
+  collapsedSteps: number[] = [2, 3, 4];
+  isCollapsed(stepNumber: number): boolean {
+    return this.visibleStep !== stepNumber;
+  }
 
+  toggleStep(stepNumber: number) {
+    this.visibleStep = this.visibleStep === stepNumber ? this.visibleStep : stepNumber;
+  }
+
+  gotoStep(stepNumber: number) {
+    this.visibleStep = stepNumber;
+  }
 }

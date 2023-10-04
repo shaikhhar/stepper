c1eaa80dd4df24504d5a72f94b222e33879e00a3
shekhar <skrbasnet@gmail.com>
2023-10-04 08:08:16 +0545

    progress till now - landing page completed

---
+++ src/app/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StepperComponent } from './stepper/stepper.component';
+
+
+
+@NgModule({
+  declarations: [
+    StepperComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LeftComponent } from './questions/left/left.component';
+import { RightComponent } from './questions/right/right.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'questions', component: QuestionsComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    QuestionsComponent,
+    LeftComponent,
+    RightComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    
+  ]
+})
+export class PagesModule { }

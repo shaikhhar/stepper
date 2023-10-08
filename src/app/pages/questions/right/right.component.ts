4d8be4be49e7f6c3404737d70201927ef6aca0a0
shekhar <skrbasnet@gmail.com>
2023-10-08 14:48:46 +0545

    API integration

--- src/app/pages/questions/right/right.component.ts
+++ src/app/pages/questions/right/right.component.ts
@@ -7,19 +7,38 @@ import { StepperService } from '../../../ui/stepper/stepper.service';
   styleUrls: ['./right.component.scss']
 })
 export class RightComponent implements OnInit{
-  formResponse = {};
+  formResponse: any = {};
+  formResponseKeys:any;
+  formResponseEmpty = true;
   constructor(public stepperService: StepperService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.stepperService.formSubmissionResponse$ .subscribe(fsr => {
       console.log('this.formResponse ', fsr)
-      this.formResponse = fsr;
+      if (fsr !== null) {
+        this.formResponse = fsr;
+        this.formResponseKeys = [];
+        this.formResponseKeys = Object.keys(this.formResponse);
+        console.log('formResponseKeys', this.formResponseKeys)
+      } else {
+        this.formResponse = {}; // Assign an empty object if fsr is null
+      }
+  
+      this.formResponseEmpty = this.isFormResponseEmpty();
+      console.log('this.formResponseEmpty ', this.formResponseEmpty)
       this.cdr.detectChanges();
     })
   }
 
   isFormResponseEmpty() {
-    return Object.keys(this.formResponse).length === 0;
+    console.log('formRepsonse', this.formResponse)
+    console.log('this.formResponse.length ', Object.keys(this.formResponse))
+    return Object.keys(this.formResponse)?.length === 0;
+  }
+
+  getReponseKeys() {
+    const responseKeys = Object.keys(this.formResponse)
+    console.log('responseKeys ', responseKeys)
   }
 
 }
